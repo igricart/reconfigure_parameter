2a3a4d3c7a1d92c27f85d06c9319e3886f4b18b1
Ignacio Ricart <ignacio@poli.ufrj.br>
2019-09-23 03:04:51 -0300

    first commit

---
+++ src/reconfigure_node/src/alex_example.cpp
@@ -0,0 +1,101 @@
+
+// Copyright 2016 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <chrono>
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+using namespace std::chrono_literals;
+
+/* This example creates a subclass of Node and uses a fancy C++11 lambda
+ * function to shorten the callback syntax, at the expense of making the
+ * code somewhat more difficult to understand at first glance. */
+
+class MinimalPublisher : public rclcpp::Node
+{
+public:
+  MinimalPublisher()
+  : Node("minimal_publisher"), count_(0)
+  {
+    std::cout << "Constructor called" << std::endl;
+    this->declare_parameter<int>("asdf", 10);
+    this->declare_parameter<int>("fdsa", 20);
+    this->declare_parameter<int>("my_value", 1);
+    publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+    auto timer_callback =
+      [this]() -> void {
+        auto message = std_msgs::msg::String();
+        message.data = "Hello, world! " + std::to_string(this->count_++);
+        RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+        this->publisher_->publish(message);
+        RCLCPP_INFO(this->get_logger(), "my_value: '%i'", this->get_parameter("my_value").as_int());
+        RCLCPP_INFO(this->get_logger(), "asdf: '%i'", this->get_parameter("asdf").as_int());
+        RCLCPP_INFO(this->get_logger(), "fdsa: '%i'", this->get_parameter("fdsa").as_int());
+
+        if(count_ == 1)
+          this->send_parameters();
+
+      };
+    timer_ = this->create_wall_timer(500ms, timer_callback);
+  }
+  void send_parameters()
+  {
+    // if (this->get_parameter("my_value").as_int() != 0)
+    // {
+
+      RCLCPP_INFO(this->get_logger(), "AAA");
+      
+      auto my_value = this->get_parameter("my_value").as_int();
+      this->set_parameter({"asdf", my_value });
+
+      RCLCPP_INFO(this->get_logger(), "BBB");
+      
+      auto client = this->create_client<rcl_interfaces::srv::SetParameters>("/minimal_publisher/set_parameters");
+      auto request = std::make_shared<rcl_interfaces::srv::SetParameters::Request>();
+      auto param = rcl_interfaces::msg::Parameter();
+      param.name = "fdsa";
+      param.value.type = 2;
+      param.value.integer_value = my_value;
+      request->parameters.push_back(param);
+
+      RCLCPP_INFO(this->get_logger(), "CCC");
+      
+      client->async_send_request(request);
+      RCLCPP_INFO(this->get_logger(), "DDD");
+    // }
+  }
+
+private:
+  rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+  size_t count_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<MinimalPublisher>();
+  rclcpp::executors::SingleThreadedExecutor exe;
+  exe.add_node(node);
+  exe.spin_some();
+  // node->send_parameters();
+  exe.spin();
+  // exe.spin_node_some(node->get_node_base_interface());
+  // rclcpp::spin(mynode);
+  rclcpp::shutdown();
+  return 0;
+}
---
+++ src/reconfigure_node/src/simple_node.cpp
@@ -0,0 +1,111 @@
+
+// Copyright 2016 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <chrono>
+#include <memory>
+#include <vector>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+using namespace std::chrono_literals;
+
+/* This example creates a subclass of Node and uses a fancy C++11 lambda
+ * function to shorten the callback syntax, at the expense of making the
+ * code somewhat more difficult to understand at first glance. */
+
+class SimpleNode : public rclcpp::Node
+{
+public:
+  SimpleNode()
+  : Node("simple_node",
+      rclcpp::NodeOptions()
+        .allow_undeclared_parameters(true)
+        .automatically_declare_parameters_from_overrides(true)
+        .start_parameter_event_publisher(true)
+    ),
+    count_(0)
+  {
+    std::cout << "Constructor called" << std::endl;
+    // this->declare_parameter<int>("my_value", 100);
+    
+    auto client = this->create_client<rcl_interfaces::srv::SetParameters>(std::string(this->get_name()) + "/set_parameters");
+
+    auto param_names = this->list_parameters(
+      {},
+      rcl_interfaces::srv::ListParameters::Request::DEPTH_RECURSIVE
+    );
+
+    std::vector<rclcpp::Parameter> params;
+    std::vector<std::string> params_names_vector(param_names.names.size());
+
+    std::cout << "Parameters in server: " << param_names.names.size() << std::endl;
+    for(size_t i = 0; i < param_names.names.size(); ++i)
+      params_names_vector[i] = param_names.names[i];
+
+    std::cout << "After client declaration" << std::endl; 
+    for(auto& v : params_names_vector)
+      std::cout << this->get_parameter(v).get_name() << " - " << this->get_parameter(v).value_to_string() << std::endl;
+
+    auto request = std::make_shared<rcl_interfaces::srv::SetParameters::Request>();
+    auto param = rcl_interfaces::msg::Parameter();
+    param.name = "my_value";
+    param.value.type = 2;
+    param.value.integer_value = this->get_parameter("my_value").as_int();
+    // param.value.integer_value = 12345;
+    // Ways to populate request message
+    // 1.
+    request->parameters.push_back(param);
+
+    // 2.
+    // request->parameters[0] = param;
+
+
+    // Ways to send parameters
+    // 1.
+    // client->async_send_request(request);
+
+    // 2
+    std::cout << "Just before sending parameters" << std::endl;
+    auto result_future = client->async_send_request(request);
+    if (rclcpp::spin_until_future_complete(this->get_node_base_interface(), result_future) !=
+    rclcpp::executor::FutureReturnCode::SUCCESS)
+    {
+      RCLCPP_ERROR(this->get_logger(), "service call failed :(");
+    }
+    auto result = result_future.get();
+    std::cout << "Result: " << result->results[0].successful << std::endl;
+    std::cout << "After sending request" << std::endl;
+    std::cout << this->get_name() << " - " << this->get_parameter("my_value").as_int() << std::endl;
+  }
+
+private:
+  size_t count_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<SimpleNode>();
+  rclcpp::executors::SingleThreadedExecutor exe;
+  exe.add_node(node);
+  exe.spin_some();
+  // node->send_parameters();
+  exe.spin();
+  // exe.spin_node_some(node->get_node_base_interface());
+  // rclcpp::spin(mynode);
+  rclcpp::shutdown();
+  return 0;
+}
